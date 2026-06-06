853fa31f3922c23cf0c0fd3ca86f881433085e17
Otto Freund <ottofreund05@gmail.com>
2026-06-06 13:07:04 +0300

    Fixed: Perft tests called with wrong parameter, causing tests failing. Fixed now.

--- tests/perft_tests.rs
+++ tests/perft_tests.rs
@@ -8,26 +8,26 @@ use std::time::{Duration, Instant};
 fn default_pos_perft_correct() {
     let move_gen = MoveGen::init();
     let mut pos: Position = Position::position_with(DEFAULT_FEN, &move_gen).unwrap();
-    //assert_eq!(go_perft(2, &mut pos), 400);
-    //assert_eq!(go_perft(3, &mut pos), 8902)
-    //assert_eq!(go_perft(4, &mut pos), 197281);
-    //perft_logger(5, &mut pos, Some(4));
-    //assert_eq!(go_perft(5, &mut pos), 4865609);
-    //assert_eq!(go_perft(6, &mut pos, &move_gen), 119060324);
+    //assert_eq!(go_perft(2, &mut pos, &move_gen), 400);
+    //assert_eq!(go_perft(3, &mut pos, &move_gen), 8902)
+    //assert_eq!(go_perft(4, &mut pos, &move_gen), 197281);
+    //perft_logger(5, &mut pos, Some(4), &move_gen);
+    //assert_eq!(go_perft(5, &mut pos, &move_gen), 4865609);
+    assert_eq!(go_perft(6, &mut pos, &move_gen), 119060324);
     
-    perft_benchmark(|| {return go_perft(6, &mut pos, &move_gen);});
+    //perft_benchmark(|| {return go_perft(6, &mut pos, &move_gen);});
 }
 
 #[test]
 fn edge_case_perft_2() { //"kiwipete" position
     let move_gen = MoveGen::init();
     let mut pos: Position = Position::position_with("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ", &move_gen).unwrap();
-    //assert_eq!(go_perft(2, &mut pos), 2039);
-    //pos.try_make_move(4, 6).unwrap();
-    //pos.try_make_move(23, 14).unwrap();
-    //perft_logger(2, &mut pos, Some(1));
-    //assert_eq!(go_perft(3, &mut pos), 97862);
-    //assert_eq!(go_perft(4, &mut pos), 4085603);
+    //assert_eq!(go_perft(2, &mut pos, &move_gen), 2039);
+    //pos.try_make_move(4, 6, &move_gen).unwrap();
+    //pos.try_make_move(23, 14, &move_gen).unwrap();
+    //perft_logger(2, &mut pos, Some(1), &move_gen);
+    //assert_eq!(go_perft(3, &mut pos, &move_gen), 97862);
+    //assert_eq!(go_perft(4, &mut pos, &move_gen), 4085603);
     assert_eq!(go_perft(5, &mut pos, &move_gen), 193690690);
     //perft_benchmark(|| {return go_perft(4, &mut pos, &move_gen);});
 }
@@ -36,24 +36,24 @@ fn edge_case_perft_2() { //"kiwipete" position
 fn edge_case_perft_3() {
     let move_gen = MoveGen::init();
     let mut pos: Position = Position::position_with("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", &move_gen).unwrap();
-    //assert_eq!(go_perft(2, &mut pos), 191);
-    //assert_eq!(go_perft(3, &mut pos), 2812)
-    //assert_eq!(go_perft(4, &mut pos), 43238);
-    //perft_logger(2, &mut pos, Some(1));
-    //assert_eq!(go_perft(5, &mut pos), 674624);
-    //assert_eq!(go_perft(6, &mut pos), 11030083);
-    //assert_eq!(go_perft(7, &mut pos), 178633661);
-    perft_benchmark(|| {return go_perft(7, &mut pos, &move_gen);});
+    //assert_eq!(go_perft(2, &mut pos, &move_gen), 191);
+    //assert_eq!(go_perft(3, &mut pos, &move_gen), 2812)
+    //assert_eq!(go_perft(4, &mut pos, &move_gen), 43238);
+    //perft_logger(2, &mut pos, Some(1), &move_gen);
+    //assert_eq!(go_perft(5, &mut pos, &move_gen), 674624);
+    //assert_eq!(go_perft(6, &mut pos, &move_gen), 11030083);
+    assert_eq!(go_perft(7, &mut pos, &move_gen), 178633661);
+    //perft_benchmark(|| {return go_perft(7, &mut pos, &move_gen);});
 }
 
 #[test]
 fn edge_case_perft_4() {
     let move_gen = MoveGen::init();
     let mut pos: Position = Position::position_with("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", &move_gen).unwrap();
-    //assert_eq!(go_perft(2, &mut pos), 264);
-    //assert_eq!(go_perft(3, &mut pos), 9467)
-    //assert_eq!(go_perft(4, &mut pos), 422333);
-    //perft_logger(2, &mut pos, Some(1));
+    //assert_eq!(go_perft(2, &mut pos, &move_gen), 264);
+    //assert_eq!(go_perft(3, &mut pos, &move_gen), 9467)
+    //assert_eq!(go_perft(4, &mut pos, &move_gen), 422333);
+    //perft_logger(2, &mut pos, Some(1), &move_gen);
     assert_eq!(go_perft(5, &mut pos, &move_gen), 15833292);
 }
 
@@ -62,10 +62,10 @@ fn edge_case_perft_4() {
 fn edge_case_perft_5() {
     let move_gen = MoveGen::init();
     let mut pos: Position = Position::position_with("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", &move_gen).unwrap();
-    //assert_eq!(go_perft(2, &mut pos), 1486);
-    //assert_eq!(go_perft(3, &mut pos), 62379)
-    //assert_eq!(go_perft(4, &mut pos), 2103487);
-    //perft_logger(2, &mut pos, Some(1));
+    //assert_eq!(go_perft(2, &mut pos, &move_gen), 1486);
+    //assert_eq!(go_perft(3, &mut pos, &move_gen), 62379)
+    //assert_eq!(go_perft(4, &mut pos, &move_gen), 2103487);
+    //perft_logger(2, &mut pos, Some(1), &move_gen);
     assert_eq!(go_perft(5, &mut pos, &move_gen), 89941194);
 }
 
@@ -73,10 +73,10 @@ fn edge_case_perft_5() {
 fn edge_case_perft_6() {
     let move_gen = MoveGen::init();
     let mut pos: Position = Position::position_with("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", &move_gen).unwrap();
-    //assert_eq!(go_perft(2, &mut pos), 2079);
-    //assert_eq!(go_perft(3, &mut pos), 89890)
-    //assert_eq!(go_perft(4, &mut pos), 3894594);
-    //perft_logger(2, &mut pos, Some(1));
+    //assert_eq!(go_perft(2, &mut pos, &move_gen), 2079);
+    //assert_eq!(go_perft(3, &mut pos, &move_gen), 89890)
+    //assert_eq!(go_perft(4, &mut pos, &move_gen), 3894594);
+    //perft_logger(2, &mut pos, Some(1), &move_gen);
     assert_eq!(go_perft(5, &mut pos, &move_gen), 164075551);
 }
 
@@ -92,7 +92,7 @@ fn go_perft(target_depth: usize, pos: &mut Position, move_gen: &MoveGen) -> u32 
         let (s, e) = pos.search_move_bounds();
         for i in s..e {
             let mov: u32 = pos.move_arr[i];
-            pos.make_move(mov, true, false, &move_gen);
+            pos.make_move(mov, true, true, &move_gen);
             from_here += inner(d_left - 1, pos, move_gen);
             pos.unmake_move(mov);
         }
