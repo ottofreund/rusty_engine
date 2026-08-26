f9fb63b059ceaf1562d352a7e52ee765445a23a6
Otto Freund <ottofreund05@gmail.com>
2026-08-26 15:25:20 +0300

    Undo regression

--- tests/search_tests.rs
+++ tests/search_tests.rs
@@ -18,7 +18,6 @@ use std::sync::{
 };
 
 const MATE_IN_ONE_FEN: &str = "7k/8/5KQ1/8/8/8/8/8 w - - 0 1";
-const QUIET_FIFTY_MOVE_FEN: &str = "7r/5k2/8/8/8/8/2K5/R7 w - - 99 1";
 const STOP_CHECK_INTERVAL_NODES: u64 = 8192;
 const CANCEL_TEST_DEPTH: usize = 6;
 
@@ -78,29 +77,6 @@ fn static_depth_quiescence_rejects_poisoned_capture() {
     );
 }
 
-#[test]
-fn quiescence_scores_quiet_fifty_move_children_as_draws() {
-    let engine = TestEngine::new();
-    let start = engine.position(QUIET_FIFTY_MOVE_FEN);
-
-    assert!(!start.legal_moves().is_empty());
-    for &mov in start.legal_moves() {
-        assert!(!_move::is_eating(mov));
-        assert!(!_move::is_promotion(mov));
-
-        let mut child = start.clone();
-        engine.make_search_move(&mut child, mov);
-        assert!(child.board.is_fifty_move_draw());
-    }
-
-    let searcher = search_static_depth(&engine, QUIET_FIFTY_MOVE_FEN, 1, true);
-    let root_entry = searcher
-        .tt
-        .probe(start.zhash)
-        .expect("completed root search should be stored in the TT");
-
-    assert_eq!(root_entry.score, 0);
-}
 
 #[test]
 fn static_depth_quiescence_preserves_nominal_pv_length() {
