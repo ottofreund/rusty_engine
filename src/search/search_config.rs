246acc07e0134ac430d8e52053cb49d74e981482
Otto Freund <ottofreund05@gmail.com>
2026-08-26 23:25:38 +0300

    New time mgmt was timing out when very low on time. Fixed now. Also debug build margin bumped up a little for GitHub actions

--- src/search/search_config.rs
+++ src/search/search_config.rs
@@ -1,5 +1,5 @@
 #[cfg(debug_assertions)]
-const SEARCH_RESPONSE_MARGIN: u64 = 50; //ms
+const SEARCH_RESPONSE_MARGIN: u64 = 65; //ms
 #[cfg(not(debug_assertions))]
 const SEARCH_RESPONSE_MARGIN: u64 = 15; //ms
 
@@ -58,7 +58,7 @@ impl SearchMode {
     ) -> Self {
         let time_left = if is_white_turn { wtime } else { btime };
         let inc = if is_white_turn { winc } else { binc };
-        let time_for_move = (time_left / 20).saturating_add(inc);
+        let time_for_move = ((time_left / 20).saturating_add(inc)).min(time_left);
         return Self::StaticTime(time_for_move.saturating_sub(SEARCH_RESPONSE_MARGIN));
     }
 }
