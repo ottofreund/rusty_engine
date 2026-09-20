80f79e8823cff7fb65b99a2bb1148e438d960737
Otto Freund <ottofreund05@gmail.com>
2026-09-20 11:03:22 +0300

    change run mode

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use iced::futures::executor::block_on;
 use rusty_engine::{game::cpu_game, repr::types::VERSION, uci::command_listener::listen, ui::app};
 
 fn main() {
-    let uci_mode: bool = true;
+    let uci_mode: bool = false;
     if uci_mode {
         println!("Rusty Engine v{} by Otto Freund", VERSION);
         let cpu_game = cpu_game::CpuGame::default();
